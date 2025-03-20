0213197ed80ad9249836f62a8eb126346114b282
Lauren Bushman <lkf57@nau.edu>
2025-03-19 17:45:03 -0700

    fixing a commit without adding cuz im dumb

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import { Toaster } from "@/components/ui/toaster"
 import './globals.css';
+import { AuthProvider } from "@/context/AuthContext";
 
 const geistSans = localFont({
 	src: './fonts/GeistVF.woff',
@@ -29,7 +30,10 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				{children}
+				<AuthProvider>
+					{children}
+				</AuthProvider>
+
 				<Toaster />
 			</body>
 		</html>
